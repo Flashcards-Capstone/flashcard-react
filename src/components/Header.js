1defdc26a6e25743a334ea5e62616caa48debd0c
Micha Bernhard <mib70@pitt.edu>
2023-02-09 19:03:32 -0700

    Add redirection to button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// Make the button disappear if the user is loggen in
+// Make the button disappear if the user is logged in
 
 const Header = () => {
+    const [showButton, setShowButton] = useState(false);
+
+    const changeButtonvisibility = () => {
+        setShowButton(!showButton);
+        window.location.replace('http://localhost:3000/login');
+    }
+
     return (
         <nav class="navbar bg-dark" data-bs-theme="dark">
             <div class="container-fluid">
                 <h1 class="text-light bg-dark">FlashCard</h1>
                 <span class="navbar-text">
-                    <button type="button" class="btn btn-success">Log In</button>
+                    <scrpit>
+                        <button type="button" class="btn btn-success" onClick={() => changeButtonvisibility()}>Log In</button>
+                    </scrpit>
                 </span>
             </div>
         </nav>
