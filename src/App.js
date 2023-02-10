0c61baefe83dc1801f358b7e00c2dc912094d500
Micha Bernhard <mib70@pitt.edu>
2023-02-09 19:03:15 -0700

    Add login route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Header from './components/Header';
+import LogIn from './components/LogIn';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={ <Home/> } exact />
+        <Route path="/login" element={ <LogIn/> } exact />
       </Routes>
 
     </div>
